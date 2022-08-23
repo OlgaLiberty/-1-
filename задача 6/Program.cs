fba91670633f831b235531b08dc82b75a58008f3
Olga <kirillovaolya@gmail.com>
2022-08-23 17:25:15 +0400

    add 6

---
+++ задача 6/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите число:");
+           int a = Convert.ToInt32(Console.ReadLine());
+
+            if (a % 2 == 1)
+            {
+                Console.WriteLine("Число " + a + " нечетное");
+            }
+            else
+            {
+                Console.WriteLine("Число " + a + "четное");
+            }
